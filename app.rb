87b26a497d620bcf0abe86f3f1982e9ada51bb7f
KatIsCoding <fabrigoru@gmail.com>
2022-01-25 13:20:36 -0600

    Add database file

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require_relative 'ui'
+require './database.rb'
 
 class App
+  database_object = Database.new()
   include UI
 end
---
+++ database.rb
@@ -0,0 +1,24 @@
+class Database
+  def initialize()
+    @data = {}
+  end
+  ::attr_reader :data
+
+  def add_to_database(item)
+    item_name = item.name.to_s
+    if @data.keys().include?(item_name)
+      @data[item_name].append(item)
+    else
+      @data[item_name] = [item]
+    end
+  end
+
+  def get_items(type)
+    if @data.keys().include?(type)
+      return @data[type]
+    else
+      return []
+  end
+
+
+end
