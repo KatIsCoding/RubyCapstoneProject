d385966df2d3a54e3af01be69a35b936dea0d56c
KatIsCoding <fabrigoru@gmail.com>
2022-01-25 12:25:05 -0600

    Book Method Override

---
+++ book.rb
@@ -0,0 +1,13 @@
+require './item'
+
+class Book < Item
+  def initialize(id:, archived:, date:, publisher:, cover_state:)
+    super(id, archived, date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?()
+    super.can_be_archived? || @cover_state == 'bad'
+  end
+end
