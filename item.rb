b489960c58378e18fb886655cc3b549fa6495ae9
KatIsCoding <fabrigoru@gmail.com>
2022-01-25 09:29:35 -0600

    Base Item class

---
+++ item.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'date'
+
+# Item class
+class Item
+  def initialize(id:, archived:, date:)
+    @id = id
+    @archived = archived
+    # Be always a Date object
+    @publish_date = date
+  end
+
+  def add_genre(genre_obj)
+    @genre = genre_obj
+  end
+
+  def add_author(author_obj)
+    @author = author_obj
+  end
+
+  def add_source(source_obj)
+    @source = source_obj
+  end
+
+  def add_label(label_obj)
+    @label =  label_obj
+  end
+
+  def can_be_archived?
+    now = Date.today
+    diff = (now - @publish_date).to_i
+    (diff / 365.25).to_i > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
