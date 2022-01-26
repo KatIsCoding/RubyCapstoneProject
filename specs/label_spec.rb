f5ca8008c4e74aa5ed54f883644d0cd7ea11afc1
KatIsCoding <fabrigoru@gmail.com>
2022-01-26 15:26:23 -0600

    Label tests added

---
+++ specs/label_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../label.rb'
+require_relative '../book.rb'
+
+describe Label do 
+  before(:each) do
+    @obj = Label.new(title: "Cool label", color: "red")
+  end
+
+  context 'Label instantiation' do
+    it '#new' do
+      expect(@obj).to be_a Label
+    end
+  end
+
+  context 'Test add_item Method' do
+    it 'Execute Method' do
+      mock = Book.new(date: '2020/01/01', publisher: "Me", cover_state: "bad")
+      @obj.add_item(mock)
+      expect(@obj.instance_variable_get("@items").length).to be(1)
+      expect(mock.instance_variable_get("@label")).to be(@obj)
+    end
+  end
+
+end
